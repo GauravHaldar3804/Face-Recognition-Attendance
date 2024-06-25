7727d4d32f64864e2b677a38274a2625c3381ad7
Gaurav Haldar <haldargaurav04@gmail.com>
2024-06-25 21:05:10 +0530

    Added image overlays

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+import os
 
 cap = cv.VideoCapture(0)
 
@@ -7,9 +8,23 @@ cap.set(4,480)
 
 imgbackground = cv.imread("Resources/background.png")
 
+# Importing the Mode Images to the List
+folderPath = "Resources/Modes"
+modePathList = os.listdir(folderPath)
+
+imgModeList = []
+
+for path in modePathList:
+    imgModeList.append(cv.imread(os.path.join(folderPath,path)))
+
+
 while True:
     success , img = cap.read()
-    cv.imshow("Webcam", img)
+    img = cv.flip(img,1)
+
+    imgbackground[162:162+480 , 55:55+640 ] = img
+    imgbackground[44:44+633 , 808:808+414 ] = imgModeList[3]
+
     cv.imshow("Face Attendance", imgbackground)
 
     cv.waitKey(1)
