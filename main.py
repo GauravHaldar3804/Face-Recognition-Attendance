1dc850e57a6ae1fcf4df109b0273b5c32075b038
Gaurav Haldar <haldargaurav04@gmail.com>
2024-06-25 22:56:41 +0530

    Face Recognition Started

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import os
+import pickle
+import face_recognition as fg
 
 cap = cv.VideoCapture(0)
 
@@ -17,13 +19,32 @@ imgModeList = []
 for path in modePathList:
     imgModeList.append(cv.imread(os.path.join(folderPath,path)))
 
+# Load Encode File
+print("Loading Encoded File ...........")
+file = open("EncodeFile.p","rb")
+encodeListKnownWithIds = pickle.load(file)
+file.close()
+encodeListKnown , studentIds = encodeListKnownWithIds
+print(" Encoded File Loaded")
 
 while True:
     success , img = cap.read()
     img = cv.flip(img,1)
 
+    imgS = cv.resize(img , (0,0) , None , 0.25 , 0.25)
+    imgS = cv.cvtColor(imgS , cv.COLOR_BGR2RGB)
+
+    faceCurrFrame = fg.face_locations(imgS)
+    encodeCurrFrame = fg.face_encodings(imgS,faceCurrFrame)
+
     imgbackground[162:162+480 , 55:55+640 ] = img
     imgbackground[44:44+633 , 808:808+414 ] = imgModeList[1]
+
+    for encodeFace , faceLoc in zip(encodeCurrFrame,faceCurrFrame):
+        matches = fg.compare_faces(encodeListKnown,encodeFace)
+        faceDis = fg.face_distance(encodeListKnown,encodeFace)
+        print("Matches",matches)
+        print("FaceDis",faceDis)
 
     cv.imshow("Face Attendance", imgbackground)
 
