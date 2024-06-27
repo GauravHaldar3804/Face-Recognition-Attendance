80ddb8e2c705d2caf2a94c308d5ed454e3ff924c
Gaurav Haldar <haldargaurav04@gmail.com>
2024-06-27 17:02:23 +0530

    Face recognition Done

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2 as cv
 import os
+import numpy as np
+import cvzone
 import pickle
 import face_recognition as fg
 
@@ -43,8 +45,23 @@ while True:
     for encodeFace , faceLoc in zip(encodeCurrFrame,faceCurrFrame):
         matches = fg.compare_faces(encodeListKnown,encodeFace)
         faceDis = fg.face_distance(encodeListKnown,encodeFace)
-        print("Matches",matches)
-        print("FaceDis",faceDis)
+        
+
+        matchIndex = np.argmin(faceDis)
+        y1 , x2 , y2 , x1 = faceLoc
+        y1 , x2 , y2 , x1 = y1*4 , x2*4 , y2*4 , x1*4
+        bbox = 55 + x1 , 162 + y1 , x2 - x1 , y2 - y1
+        imgbackground = cvzone.cornerRect(imgbackground,bbox,rt = 0)
+
+        if matches[matchIndex]:
+            print(studentIds[matchIndex])
+                 
+        if not matches[matchIndex]:
+            print("Unknown Face")
+            
+            
+
+
 
     cv.imshow("Face Attendance", imgbackground)
 
