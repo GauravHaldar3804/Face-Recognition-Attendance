61ddd98ff6a028bd69d0446b6c9279200ec455c2
Gaurav Haldar <haldargaurav04@gmail.com>
2024-06-27 17:28:29 +0530

    Setting up Database

--- AddDataTodatabase.py
+++ AddDataTodatabase.py
@@ -1,5 +1,45 @@
 import firebase_admin
 from firebase_admin import credentials
+from firebase_admin import db
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,{
+    "databaseURL":"https://facerecognitionrealtime-bd2e2-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "187080":
+    {
+        "Name":"Gaurav Haldar",
+        "Major":"Mechatronics",
+        "Starting_Year":2022,
+        "Total_Attendance":34,
+        "Standing":"Good",
+        "Year":2,
+        "Last_Attendance_time":"2024-4-29 00:54:34"
+    },
+    "268465":
+    {
+        "Name":"Akshay Kumar",
+        "Major":"BMM",
+        "Starting_Year":2021,
+        "Total_Attendance":45,
+        "Standing":"Good",
+        "Year":3,
+        "Last_Attendance_time":"2024-4-14 00:54:34"
+    },
+    "458798":
+    {
+        "Name":"Priyanka Chopra",
+        "Major":"Fashion Designing",
+        "Starting_Year":2020,
+        "Total_Attendance":64,
+        "Standing":"Bad",
+        "Year":4,
+        "Last_Attendance_time":"2023-5-29 00:54:34"
+    }
+}
+for key , value in data.items():
+    ref.child(key).set(value)
